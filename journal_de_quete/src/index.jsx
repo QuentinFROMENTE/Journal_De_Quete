a45ef4e300d096df2db5f92f9e3c9bf62bbc040f
QuentinFROMENTE <fromentequentin@gmail.com>
2023-03-19 23:14:58 +0100

    Ajout de la page Game.jsx Mise en place du routage post connexion

--- journal_de_quete/src/index.jsx
+++ journal_de_quete/src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 import './css/css/style.css';
 import Acceuil from './pages/acceuil';
 import Creation from './pages/creation';
+import Chargement from './pages/chargement';
+import Game from './pages/game';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +14,10 @@ root.render(
     <Routes>
       <Route path="/" element={<Acceuil/>} />
       <Route path="/NewSave" element={<Creation/>} />
+      <Route path="/LoadSave" element={<Chargement/>} />
+      <Route path="/:profilName">
+        <Route path="/:profilName/" element={<Game/>}/>
+      </Route>
     </Routes>
   </Router>
 );
---
+++ journal_de_quete/src/pages/game.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import '../css/css/style.css';
+
+export default function Game() {
+
+    return (
+        <p>page game</p>
+    )
+}
