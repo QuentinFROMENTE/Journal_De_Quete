86f4f6a1f9207174c4d53b50f2d93f6a1e4edf1f
QuentinFROMENTE <fromentequentin@gmail.com>
2023-03-21 15:31:50 +0100

    retrait du useParams inutilisé

--- journal_de_quete/src/index.jsx
+++ journal_de_quete/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/css/style.css';
 import Acceuil from './pages/acceuil';
 import Creation from './pages/creation';
