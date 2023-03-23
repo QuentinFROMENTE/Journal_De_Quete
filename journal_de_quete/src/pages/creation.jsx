5327b3b9c3b772b2102155ff87c6dde1523da120
QuentinFROMENTE <fromentequentin@gmail.com>
2023-03-23 18:16:37 +0100

    implémentation de l'AlertBox

--- journal_de_quete/src/pages/creation.jsx
+++ journal_de_quete/src/pages/creation.jsx
@@ -1,29 +1,43 @@
 import React from "react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { CreateCookie, ReadCookie } from "../js/utils";
 import Utilisateur from "../js/cookie";
+import AlertBox from "../composants/alertBox";
 
 export default function Creation() {
 
     const playerName = useRef("");
+    const [isAllReadyUsed, setIsAllReadyUsed] = useState(false);
 
     function AddProfil() {
         
         let player = playerName.current.value;
 
         if (document.cookie) {
-            let cookie = JSON.parse(ReadCookie());
+            let cookie = ReadCookie();
+
             /*faire une vérification de l'inexistance du nouveau profil*/
-            
-            cookie.DATA.push(new Utilisateur(player));
-            CreateCookie(cookie);
+
+            let isExist = 0;
+            for ( let i = 0; i < cookie.DATA.length; i++) {
+                if (player === cookie.DATA[i].name) {
+                    isExist = 1;
+                }
+            }
+
+            /*Alerte si Pseudo déjà utiliser sinon enregistrement*/
+
+            if (isExist !== 0) {
+                setIsAllReadyUsed(true);
+            } else {
+                cookie.DATA.push(new Utilisateur(player));
+                CreateCookie(cookie);
+            }
 
         } else {
-            let newSave = {DATA: [new Utilisateur(player)]};
-            let cookie = JSON.stringify(newSave);
+            let cookie = {DATA: [new Utilisateur(player)]};
             CreateCookie(cookie);
         }
-      
     }
 
     return (
@@ -33,6 +47,7 @@ export default function Creation() {
                 <input type="text" id="name" name="user_name" className="creation__input" ref={playerName}/>
             </form>
             <button onClick={AddProfil} className="button button__navigation button__navigation--validation">Créer Sauvegarde</button>
+            {isAllReadyUsed ? <AlertBox string="Pseudo déjà utilisé"/> : null}
         </>
     )
 }
