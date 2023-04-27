c5dd8a5cf3c6876697e665033a229c4b88d6e29b
QuentinFROMENTE <fromentequentin@gmail.com>
2023-04-27 15:08:45 +0200

    Réalisation de la page de listing de type de quête

--- journal_de_quete/src/pages/listTypeQuest.jsx
+++ journal_de_quete/src/pages/listTypeQuest.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import {useParams} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom';
+import NavigationLong from '../composants/navigationLong';
 
-export default function ListTypeQuest(props){
+export default function ListTypeQuest(){
+
     const {profilName, game} = useParams();
+    const dataGame = require(`../database/Games/${game}.json`);
+
+    let typeQuestArray = [];
+    for (let typeQuest in dataGame.questList) {
+        typeQuestArray.push(dataGame.questList[typeQuest]);
+    }
+
     return(
         <>
-            <p>partie de {game} de {profilName}</p>
+            <NavigationLong string={dataGame.name}/>
+            {typeQuestArray.map((data, index)=>{
+                return <Link to={`/${profilName}/${game}/${data.URL}`} key={index} className="button button__navigation">{data.titleType}</Link>
+            })}
         </>
     )
 }
